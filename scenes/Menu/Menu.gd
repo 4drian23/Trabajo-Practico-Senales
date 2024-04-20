f0adcfa7f94c18af3e31c0131143140f1cae88c7
4drian23 <163658879+4drian23@users.noreply.github.com>
2024-04-20 11:50:18 -0300

    Funcion “count_new_instance” creada

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -7,5 +7,9 @@ var instancesCount = 0
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
-	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount);
 	pass
+
+func count_new_instance():
+	instancesCount += 1;
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount);
